8c291ff186fa8eea948447e95b27d0158e6d3797
Martin Raspaud <martin.raspaud@smhi.se>
2026-09-11 16:55:17 +0200

    Drop the GPL header from the files lac2pps adds
    level1c4pps is moving to Apache 2, so the new lac2pps script, library and
    tests come without the GPL notice. Files that existed before keep theirs
    until the project-wide change. Comments only; no test covers them.

--- level1c4pps/lac2pps_lib.py
+++ level1c4pps/lac2pps_lib.py
@@ -1,21 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright (c) 2026 level1c4pps developers
-#
-# This file is part of level1c4pps
-#
-# level1c4pps is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# level1c4pps is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with level1c4pps.  If not, see <http://www.gnu.org/licenses/>.
 
 """Convert AVHRR LAC and FRAC data to PPS level-1c format."""
 
